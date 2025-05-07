34a40014e06d9a264a805f21d4fd5189da413f18
FabioMorrone <morronefabio04@gmail.com>
2025-05-07 18:04:45 +0200

    fix Footer component structure and update CSS for improved layout and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,41 @@
 export default function Footer() {
-  return (
-    <>
-      <footer>
-        <div className="container">
-          <div className="col col-xl-">
-            <div>
-              <ul className="nav justify-content-start flex-column ">
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Saperne di più
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Contattaci
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    FunkoInfo
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Lavora con noi
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Scegli il tuo Funko
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </>
-  );
+    return (
+        <>
+            <footer>
+                <div className="container">
+                    <div className="col col-xl-12">
+                        <div>
+                            <ul className="nav justify-content-start flex-column ">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">
+                                        Saperne di più
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">
+                                        Contattaci
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">
+                                        FunkoInfo
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">
+                                        Lavora con noi
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">
+                                        Scegli il tuo Funko
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
 }
