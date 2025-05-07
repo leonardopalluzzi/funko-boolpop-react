318238ab5cb80db531da5be65dab0b9a9ef34908
RosyPetrella <petrellamr@gmail.com>
2025-05-07 17:51:48 +0200

    Edit Footer and Header components for improved structure and styling; update CSS for header and footer backgrounds

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
 export default function Header() {
-    return (
-        <>
-
-            <header>
-                <div className="container text-center">
-                    <a className="navbar-brand" href="/">
-                        <img src="/logoipsum-custom-logo-8.svg" alt="logo" />
-                    </a>
-                </div>
-            </header>
-
-        </>
-    )
+  return (
+    <>
+      <header>
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            <img src="/logoipsum-custom-logo-8.svg" alt="logo" />
+          </a>
+        </div>
+      </header>
+    </>
+  );
 }
