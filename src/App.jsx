9820a5e88248a063ac4ec603247f59a7c609c039
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-07 15:01:07 +0200

    add homepage and routes for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
+import Home from "./pages/Home"
+
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            {/* pagine qui  */}
+            <Route path="/" Component={Home} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
