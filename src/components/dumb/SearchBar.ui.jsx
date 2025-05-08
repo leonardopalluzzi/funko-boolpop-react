3aabcd143259a18d322fe6dd078d69a370397920
carlomagno-ale <carlomagno.ale@gmail.com>
2025-05-08 18:38:43 +0200

    refactor: update SearchBar component to handle search logic and pass props to SearchBarUi

--- src/components/dumb/SearchBar.ui.jsx
+++ src/components/dumb/SearchBar.ui.jsx
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function SearchBarUi({ }) {
-
-
+export default function SearchBarUi({ searchText, setSearchText, handleSearch }) {
     return (
-
-        <form className="d-flex my-2 my-lg-0">
+        <form
+            className="d-flex my-2 my-lg-0"
+            role="search"
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSearch(searchText);
+                setSearchText('');
+            }}
+        >
             <input
                 className="form-control me-sm-2"
                 type="text"
                 placeholder="Search"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
             />
             <button
-                className="btn btn-outline-success my-2 my-sm-0"
+                className="btn btn-outline-success my-2 my-sm-0 d-flex align-items-center"
                 type="submit"
             >
-                Search
+                <i className="bi bi-search me-2"></i>
             </button>
         </form>
-    )
+    );
 }
--- src/components/smart/SearchBar.jsx
+++ src/components/smart/SearchBar.jsx
@@ -1,10 +1,33 @@
-import SearchBarUi from "../dumb/SearchBar.ui";
+import React, { useState } from 'react';
+import SearchBarUi from '../dumb/SearchBar.ui';
 
-export default function SearchBar() {
+export default function SearchBar({ page = 1, limit = 10 }) {
+    const [searchText, setSearchText] = useState('');
+    const [filteredFunko, setFilteredFunko] = useState('');
+
+    function handleSearch(searchText) {
+        fetch(`http://localhost:3000/api/v1/funkoboolpop?page=${page}&limit=${limit}&search=${searchText}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setFilteredFunko({
+                    state: 'success',
+                    data: data,
+                });
+            })
+            .catch((err) => {
+                setFilteredFunko({
+                    state: 'error',
+                    message: err.message,
+                });
+            });
+    }
 
     return (
-
-        <SearchBarUi />
-    )
-
+        <SearchBarUi
+            searchText={searchText}
+            setSearchText={setSearchText}
+            handleSearch={handleSearch}
+        />
+    );
 }
