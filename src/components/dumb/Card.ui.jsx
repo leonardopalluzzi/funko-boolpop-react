278250aba731dc4e8d7e9af41a5f500f66329e73
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-08 22:32:59 +0200

    card hover style

--- src/components/dumb/Card.ui.jsx
+++ src/components/dumb/Card.ui.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import ProductImages from "../smart/ProductImages"
 
 export default function CardUi({ images, name, price, attributes, license, promotions, slug }) {
@@ -8,44 +8,46 @@ export default function CardUi({ images, name, price, attributes, license, promo
     return (
         <>
             <div className="col">
-                <div className="card w-100 h-100">
-                    <div className="card-header p-0">
-                        <ProductImages images={images} />
-                    </div>
-                    <div className="card-body p-3">
-                        <h4 className="mb-4">{name}</h4>
-                        <div className="price_section_card">
-                            {
-                                promotions.length > 0 ?
-                                    (
-                                        <>
-                                            <label>
-                                                <span className="text-danger fs-6">
-                                                    <del>{price} €</del>
-                                                </span>
-                                                <span className="price_label_card text-success">
-                                                    {
-                                                        (price * promotions[0].discount / 100).toFixed(2)
-                                                    } €
-                                                </span>
-                                            </label>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <label className="price_label_card">
-                                                {funko.result.price} €
-                                            </label>
-                                        </>
-                                    )
-                            }
+                <Link to={`/${slug}`}>
+                    <div className="card w-100 h-100">
+                        <div className="card-header p-0">
+                            <ProductImages images={images} />
                         </div>
-                        <span>{attributes}</span>
-                        <span>{license}</span>
+                        <div className="card-body p-3">
+                            <h4 className="mb-4">{name}</h4>
+                            <div className="price_section_card">
+                                {
+                                    promotions.length > 0 ?
+                                        (
+                                            <>
+                                                <label>
+                                                    <span className="text-danger fs-6">
+                                                        <del>{price} €</del>
+                                                    </span>
+                                                    <span className="price_label_card text-success">
+                                                        {
+                                                            (price * promotions[0].discount / 100).toFixed(2)
+                                                        } €
+                                                    </span>
+                                                </label>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <label className="price_label_card">
+                                                    {funko.result.price} €
+                                                </label>
+                                            </>
+                                        )
+                                }
+                            </div>
+                            <span>{attributes}</span>
+                            <span>{license}</span>
+                        </div>
+                        <div className="card-footer">
+                            <button onClick={() => navigate(`/${slug}`)} className="btn btn-primary">Buy</button>
+                        </div>
                     </div>
-                    <div className="card-footer">
-                        <button onClick={() => navigate(`/${slug}`)} className="btn btn-primary">Buy</button>
-                    </div>
-                </div>
+                </Link>
             </div>
         </>
     )
