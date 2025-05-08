7a2f2d8eae500aa82bc108d3773a414d4e3a0eda
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-08 13:15:02 +0200

    add state handling on product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,69 +4,94 @@ import { useEffect, useState } from "react"
 
 export default function ProductPage() {
 
-    const [funko, setFunkos] = useState([])
+    const [funko, setFunkos] = useState({
+        state: 'loading'
+    })
+
 
     useEffect(() => {
-
-
-        fetch('http://localhost:3000/api/v1/funkoboolpop/:slug ', {})
-            .then(res => {
-                return res.json();
-            })
+        const slug = funko.slug
+        fetch(`http://localhost:3000/api/v1/funkoboolpop/${slug}`)
+            .then(res => res.json())
             .then(data => {
-                setFunkos(data)
-                console.log(funkos);
+                setFunkos({
+                    state: 'success',
+                    data: data
+                })
+                console.log(funko);
 
+            })
+            .catch(err => {
+                setFunkos({
+                    state: 'error',
+                    message: err.message
+                })
             })
     }, [])
 
 
-    return (
-        <>
-            <main>
-                <div className="container p-4 ">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
-                        <div className="col" key={funko.slug}>
-                            <div className="card h-100 shadow-lg bg-emphasis">
-                                <div className="position-relative">
-                                    <img
-                                        className="card-img-top"
-                                        src={funko.images[0].image}
-                                        alt={funko.name}
-                                        style={{ height: '300px', objectFit: 'cover' }}
-                                    />
+    switch (funko.state) {
+        case 'loading':
+            return (
+                <>
+                    <h1>Loading...</h1>
+                </>
+            )
+        case 'error':
+            return (
+                <>
+                    <h1>{funko.state}</h1>
+                    <p>{funko.message}</p>
+                </>
+            )
+        case 'success':
+            return (
+                <>
+                    <main>
+                        <div className="container p-4 ">
+                            <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
+                                <div className="col" key={funko.slug}>
+                                    <div className="card h-100 shadow-lg bg-emphasis">
+                                        <div className="position-relative">
+                                            <img
+                                                className="card-img-top"
+                                                src={funko.images[0].image}
+                                                alt={funko.name}
+                                                style={{ height: '300px', objectFit: 'cover' }}
+                                            />
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <h1>{funko.name}</h1>
-                            <p>{funko.price}</p>
-                            <button
-                                class="btn btn-outline-success my-2 my-sm-0"
-                                type="submit">
-                                Aggiungi al carrello
-                            </button>
-                            <p>{funko.desctription}</p>
-                            <div className="accordion" id="accordionExample">
-                                <div className="accordion-item">
-                                    <h2 className="accordion-header">
-                                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            Additional Information
-                                        </button>
-                                    </h2>
-                                    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                                        <div className="accordion-body">
-                                            <p>{funko.categories}</p>
-                                            <p>{funko.licenses}</p>
-                                            <p>{funko.attributes}</p>
+                                <div className="col">
+                                    <h1>{funko.name}</h1>
+                                    <p>{funko.price}</p>
+                                    <button
+                                        class="btn btn-outline-success my-2 my-sm-0"
+                                        type="submit">
+                                        Aggiungi al carrello
+                                    </button>
+                                    <p>{funko.desctription}</p>
+                                    <div className="accordion" id="accordionExample">
+                                        <div className="accordion-item">
+                                            <h2 className="accordion-header">
+                                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                    Additional Information
+                                                </button>
+                                            </h2>
+                                            <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                                <div className="accordion-body">
+                                                    <p>{funko.categories}</p>
+                                                    <p>{funko.licenses}</p>
+                                                    <p>{funko.attributes}</p>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
+                    </main>
+                </>
+            )
+    }
 }
