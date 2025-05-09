2de17c11d9507aed225afd65954ded3bdd5c0624
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-09 18:50:17 +0200

    style: clean up code formatting and improve layout in ProductPage and Card components

--- src/components/dumb/ProductImages.ui.jsx
+++ src/components/dumb/ProductImages.ui.jsx
@@ -1,8 +1,14 @@
 export default function ProductImagesUi({ images, index }) {
+
+    console.log(images[index].image);
+    console.log(index);
+
+
+
     return (
         <>
             <div className="product_images">
-                <img className="product_image" src={images[index].image} alt="" />
+                <img className="product_image" src={`http://localhost:3000/${images[index].image}`} alt="" />
             </div >
         </>
     )
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -5,161 +5,161 @@ import ProductImages from "../components/smart/ProductImages";
 import { useCartContext } from "../contexts/cartContext";
 
 export default function ProductPage({ quantity }) {
-  const { handleCart, cart } = useCartContext();
+    const { handleCart, cart } = useCartContext();
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [funko, setFunkos] = useState({
-    state: "loading",
-  });
+    const [funko, setFunkos] = useState({
+        state: "loading",
+    });
 
-  const { slug } = useParams();
+    const { slug } = useParams();
 
-  useEffect(() => {
-    fetch(`http://localhost:3000/api/v1/funkoboolpop/${slug}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setFunkos({
-          state: "success",
-          result: data,
-        });
-      })
-      .catch((err) => {
-        setFunkos({
-          state: "error",
-          message: err.message,
-        });
-      });
-  }, []);
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/v1/funkoboolpop/${slug}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setFunkos({
+                    state: "success",
+                    result: data,
+                });
+            })
+            .catch((err) => {
+                setFunkos({
+                    state: "error",
+                    message: err.message,
+                });
+            });
+    }, []);
 
-  switch (funko.state) {
-    case "loading":
-      return (
-        <>
-          <h1>Loading...</h1>
-        </>
-      );
-    case "error":
-      return (
-        <>
-          <h1>{funko.state}</h1>
-          <p>{funko.message}</p>
-        </>
-      );
-    case "success":
-      return (
-        <>
-          <main>
-            <div className="container py-5 ">
-              <div className="row row-cols-1 row-cols-lg-2 ">
-                <div
-                  className=" col col-xs-12 col-sm-12 col-md-12 col-lg-6 "
-                  key={funko.result.slug}
-                >
-                  <div className="product_page_img border rounded-5 overflow-hidden">
-                    <ProductImages images={funko.result.images} />
-                  </div>
-                </div>
-                <div className="col">
-                  <label htmlFor="">{funko.result.license.toUpperCase()}</label>
-                  <h2 className="mb-2">{funko.result.name}</h2>
-                  <div className="price_section">
-                    {/* <h2 className="mb-4">Price:</h2> */}
-                    <div>
-                      {funko.result.promotion.length > 0 ? (
-                        <>
-                          <label>
-                            <span className="text-secondary fs-5">
-                              <del>{funko.result.price} €</del>
-                            </span>
-                            <span className={`${styles.price_label} mx-3`}>
-                              {(
-                                (funko.result.price *
-                                  funko.result.promotion[0].discount) /
-                                100
-                              ).toFixed(2)}{" "}
-                              €
-                            </span>
-                          </label>
-                        </>
-                      ) : (
-                        <>
-                          <label className="price_label">
-                            {funko.result.price} €
-                          </label>
-                        </>
-                      )}
-                    </div>
-                  </div>
-                  <button
-                    onClick={() => handleCart(funko.result)}
-                    className={`${styles.btn_add} btn btn_chart my-2 my-sm-0`}
-                    type="submit"
-                  >
-                    Add to cart
-                  </button>
-                  <span className="mx-4">{cart.message}</span>
+    switch (funko.state) {
+        case "loading":
+            return (
+                <>
+                    <h1>Loading...</h1>
+                </>
+            );
+        case "error":
+            return (
+                <>
+                    <h1>{funko.state}</h1>
+                    <p>{funko.message}</p>
+                </>
+            );
+        case "success":
+            return (
+                <>
+                    <main>
+                        <div className="container py-5 h-100">
+                            <div className="row row-cols-1 row-cols-lg-2 ">
+                                <div
+                                    className=" col col-xs-12 col-sm-12 col-md-12 col-lg-6 "
+                                    key={funko.result.slug}
+                                >
+                                    <div className="product_page_img border rounded-5 overflow-hidden">
+                                        <ProductImages images={funko.result.images} />
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <label htmlFor="">{funko.result.license.toUpperCase()}</label>
+                                    <h2 className="mb-2">{funko.result.name}</h2>
+                                    <div className="price_section">
+                                        {/* <h2 className="mb-4">Price:</h2> */}
+                                        <div>
+                                            {funko.result.promotion.length > 0 ? (
+                                                <>
+                                                    <label>
+                                                        <span className="text-secondary fs-5">
+                                                            <del>{funko.result.price} €</del>
+                                                        </span>
+                                                        <span className={`${styles.price_label} mx-3`}>
+                                                            {(
+                                                                (funko.result.price *
+                                                                    funko.result.promotion[0].discount) /
+                                                                100
+                                                            ).toFixed(2)}{" "}
+                                                            €
+                                                        </span>
+                                                    </label>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <label className="price_label">
+                                                        {funko.result.price} €
+                                                    </label>
+                                                </>
+                                            )}
+                                        </div>
+                                    </div>
+                                    <button
+                                        onClick={() => handleCart(funko.result)}
+                                        className={`${styles.btn_add} btn btn_chart my-2 my-sm-0`}
+                                        type="submit"
+                                    >
+                                        Add to cart
+                                    </button>
+                                    <span className="mx-4">{cart.message}</span>
 
-                  <span className="d-block pt-4">
-                    {" "}
-                    <i class="bi bi-box-fill"></i> Available: {quantity}
-                  </span>
+                                    <span className="d-block pt-4">
+                                        {" "}
+                                        <i class="bi bi-box-fill"></i> Available: {quantity}
+                                    </span>
 
-                  <div className="product_description">
-                    <h4>Description:</h4>
-                    <p>{funko.result.description}</p>
-                  </div>
-                  <div
-                    className="accordion my-custom-accordion"
-                    id="accordionExample"
-                  >
-                    <div className="accordion-item ">
-                      <h2 className="accordion-header ">
-                        <button
-                          className="accordion-button"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target="#collapseOne"
-                          aria-expanded="true"
-                          aria-controls="collapseOne"
-                        >
-                          Additional Information
-                        </button>
-                      </h2>
-                      <div
-                        id="collapseOne"
-                        className="accordion-collapse collapse"
-                        data-bs-parent="#accordionExample"
-                      >
-                        <div className="accordion-body">
-                          <p>
-                            <strong>Category:</strong> {funko.result.category}
-                          </p>
-                          <p>
-                            <strong>License: </strong>
-                            {funko.result.license}
-                          </p>
-                          <p>
-                            <strong>Special Attributes:</strong>
-                            <ul className="list-unstyled">
-                              {funko.result.attributes.map((item) => (
-                                <>
-                                  <li className="mx-4">
-                                    - <strong>{item.name.toUpperCase()}</strong>
-                                  </li>
-                                </>
-                              ))}
-                            </ul>
-                          </p>
+                                    <div className="product_description">
+                                        <h4>Description:</h4>
+                                        <p>{funko.result.description}</p>
+                                    </div>
+                                    <div
+                                        className="accordion my-custom-accordion"
+                                        id="accordionExample"
+                                    >
+                                        <div className="accordion-item ">
+                                            <h2 className="accordion-header ">
+                                                <button
+                                                    className="accordion-button"
+                                                    type="button"
+                                                    data-bs-toggle="collapse"
+                                                    data-bs-target="#collapseOne"
+                                                    aria-expanded="true"
+                                                    aria-controls="collapseOne"
+                                                >
+                                                    Additional Information
+                                                </button>
+                                            </h2>
+                                            <div
+                                                id="collapseOne"
+                                                className="accordion-collapse collapse"
+                                                data-bs-parent="#accordionExample"
+                                            >
+                                                <div className="accordion-body">
+                                                    <p>
+                                                        <strong>Category:</strong> {funko.result.category}
+                                                    </p>
+                                                    <p>
+                                                        <strong>License: </strong>
+                                                        {funko.result.license}
+                                                    </p>
+                                                    <p>
+                                                        <strong>Special Attributes:</strong>
+                                                        <ul className="list-unstyled">
+                                                            {funko.result.attributes.map((item) => (
+                                                                <>
+                                                                    <li className="mx-4">
+                                                                        - <strong>{item.name.toUpperCase()}</strong>
+                                                                    </li>
+                                                                </>
+                                                            ))}
+                                                        </ul>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </main>
-        </>
-      );
-  }
+                    </main>
+                </>
+            );
+    }
 }
