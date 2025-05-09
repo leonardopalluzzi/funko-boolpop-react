7ef95d58f020b37c37661962c4b59012acb67b5c
carlomagno-ale <carlomagno.ale@gmail.com>
2025-05-09 23:20:16 +0200

    style: update Jumbo component description padding and adjust auto-slide interval timing

--- src/components/smart/Jumbo.jsx
+++ src/components/smart/Jumbo.jsx
@@ -37,7 +37,7 @@ export default function Jumbo() {
             const timer = setInterval(() => {
                 setCurrentIndex((prevIndex) => prevIndex === funkos.data.length - 1 ? 0 : prevIndex + 1)
 
-            }, 100000)
+            }, 4000)
             return () => clearInterval(timer)
         }
     }, [funkos.state, currentIndex])
