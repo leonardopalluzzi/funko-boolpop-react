f002008e9c734e9ea26c3f2035f5ae4f95cb9e04
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-10 08:46:33 +0200

    feat: add empty cart message and styling for better user experience

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -30,89 +30,102 @@ export default function Cart() {
         total = total + item
     })
 
-    return (
-        <>
-            <div className="container">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Product Image</th>
-                            <th scope="col">Product Name</th>
-                            <th scope="col">Promotions</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Price*</th>
-                            <th scope="col">ACTIONS</th>
+    switch (true) {
+        case (cart.userCart.length == 0):
+            return (
+                <>
+                    <div className="container empty_cart">
+                        <h3>You don't have anything in your cart</h3>
+                        <p className="fs-2"><i class="bi bi-emoji-frown-fill"></i></p>
+                        <button onClick={() => navigate('/')} className="btn btn-primary">Home Page</button>
+                    </div>
+                </>
+            )
+        case (cart.userCart.length > 0):
+            return (
+                <>
+                    <div className="container">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Product Image</th>
+                                    <th scope="col">Product Name</th>
+                                    <th scope="col">Promotions</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Price*</th>
+                                    <th scope="col">ACTIONS</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            cart.userCart.map(item => (
-                                <>
-                                    <tr>
-                                        <td>
-                                            <div className="cart_img_container">
-                                                <img className="cart_img" src={item.images[0].image} alt="" />
-                                            </div>
-                                        </td>
-                                        <td>{item.name}</td>
-                                        <td>
-                                            {
-                                                item.promotion.length > 0 ? (<><span>{item.promotion[0].name}: </span><span>{item.promotion[0].discount}%</span></>) : (<>No promotions found</>)
-                                            }
-                                        </td>
-                                        <td>
-                                            <button onClick={() => subtractCartQuantity(item)} className="btn btn-transaprent fs-3 p-2">-</button>
-                                            X{item.cartQuantity}
-                                            <button onClick={() => addCartQuantity(item)} className="btn btn-transaprent fs-4 p-2">+</button>
-                                            <p>{cart.message}</p>
-                                        </td>
-                                        <td>
-                                            {
-                                                item.promotion.length > 0 ?
-                                                    (
-                                                        <>
-                                                            <label>
-                                                                <span className="price_label_card text-success">
-                                                                    {
-                                                                        (item.price * item.promotion[0].discount / 100).toFixed(2)
-                                                                    } €
-                                                                </span>
-                                                            </label>
-                                                        </>
-                                                    ) : (
-                                                        <>
-                                                            <label className="price_label_card">
-                                                                {item.price} €
-                                                            </label>
-                                                        </>
-                                                    )
-                                            }
-                                        </td>
-                                        <td>
-                                            <button onClick={() => deleteFromCart(item)} className="btn btn-danger mx-2"><i class="bi bi-trash"></i></button>
-                                            <button onClick={() => navigate(`/${item.slug}`)} className="btn btn-primary"><i class="bi bi-eye"></i></button>
-                                        </td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    cart.userCart.map(item => (
+                                        <>
+                                            <tr>
+                                                <td>
+                                                    <div className="cart_img_container">
+                                                        <img className="cart_img" src={item.images[0].image} alt="" />
+                                                    </div>
+                                                </td>
+                                                <td>{item.name}</td>
+                                                <td>
+                                                    {
+                                                        item.promotion.length > 0 ? (<><span>{item.promotion[0].name}: </span><span>{item.promotion[0].discount}%</span></>) : (<>No promotions found</>)
+                                                    }
+                                                </td>
+                                                <td>
+                                                    <button onClick={() => subtractCartQuantity(item)} className="btn btn-transaprent fs-3 p-2">-</button>
+                                                    X{item.cartQuantity}
+                                                    <button onClick={() => addCartQuantity(item)} className="btn btn-transaprent fs-4 p-2">+</button>
+                                                    <p>{cart.message}</p>
+                                                </td>
+                                                <td>
+                                                    {
+                                                        item.promotion.length > 0 ?
+                                                            (
+                                                                <>
+                                                                    <label>
+                                                                        <span className="price_label_card text-success">
+                                                                            {
+                                                                                (item.price * item.promotion[0].discount / 100).toFixed(2)
+                                                                            } €
+                                                                        </span>
+                                                                    </label>
+                                                                </>
+                                                            ) : (
+                                                                <>
+                                                                    <label className="price_label_card">
+                                                                        {item.price} €
+                                                                    </label>
+                                                                </>
+                                                            )
+                                                    }
+                                                </td>
+                                                <td>
+                                                    <button onClick={() => deleteFromCart(item)} className="btn btn-danger mx-2"><i class="bi bi-trash"></i></button>
+                                                    <button onClick={() => navigate(`/${item.slug}`)} className="btn btn-primary"><i class="bi bi-eye"></i></button>
+                                                </td>
 
-                                    </tr>
-                                </>
-                            ))
-                        }
+                                            </tr>
+                                        </>
+                                    ))
+                                }
 
-                    </tbody>
-                </table>
-                <div className="total">
-                    <h4>TOTAL:</h4>
-                    <h5>{total.toFixed(2)}€</h5>
-                </div>
-                <button
-                    onClick={() => navigate(`/checkout`)}
-                    class="btn btn-success fs-3 my-2 my-sm-0"
-                    type="submit">
-                    Procedi al pagamento
-                </button>
-            </div>
+                            </tbody>
+                        </table>
+                        <div className="total">
+                            <h4>TOTAL:</h4>
+                            <h5>{total.toFixed(2)}€</h5>
+                        </div>
+                        <button
+                            onClick={() => navigate(`/checkout`)}
+                            class="btn btn-success fs-3 my-2 my-sm-0"
+                            type="submit">
+                            Procedi al pagamento
+                        </button>
+                    </div>
 
-        </>
-    )
+                </>
+            )
+    }
 }
