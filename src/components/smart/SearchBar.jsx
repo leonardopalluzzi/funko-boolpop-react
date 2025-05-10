2067fe72e6f05c5f71c6c76d359fe6e8a9708e4d
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-10 17:01:19 +0200

    fix: correct input name attribute in SearchBar UI component

--- src/components/smart/SearchBar.jsx
+++ src/components/smart/SearchBar.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBarUi from '../dumb/SearchBar.ui';
 
 export default function SearchBar({ page = 1, limit = 10 }) {
+
     const [searchText, setSearchText] = useState({
         name: '',
         category: '',
@@ -9,12 +10,18 @@ export default function SearchBar({ page = 1, limit = 10 }) {
     });
 
     function handleChange(key, value) {
+        setSearchText({
+            ...searchText,
+            [key]: value
+        })
 
     }
-    const [filteredFunko, setFilteredFunko] = useState('');
+    const [filteredFunko, setFilteredFunko] = useState({
+        state: 'loading'
+    });
 
-    function handleSearch(searchText) {
-        fetch(`http://localhost:3000/api/v1/funkoboolpop?page=${page}&limit=${limit}&name=${searchText}&description=${searchText.description}&category=${searchText.category}`)
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/v1/funkoboolpop?page=${page}&limit=${limit}&name=${searchText.name}&description=${searchText.description}&category=${searchText.category}`)
             .then((res) => res.json())
             .then((data) => {
                 console.log(data);
@@ -29,15 +36,59 @@ export default function SearchBar({ page = 1, limit = 10 }) {
                     message: err.message,
                 });
             });
+    }, [searchText])
+
+    function handleSearch(searchText) {
+        // fetch(`http://localhost:3000/api/v1/funkoboolpop?page=${page}&limit=${limit}&name=${searchText.name}&description=${searchText.description}&category=${searchText.category}`)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         console.log(data);
+        //         setFilteredFunko({
+        //             state: 'success',
+        //             data: data,
+        //         });
+        //     })
+        //     .catch((err) => {
+        //         setFilteredFunko({
+        //             state: 'error',
+        //             message: err.message,
+        //         });
+        //     });
     }
 
-    return (
-        <SearchBarUi
-            serachName={searchText.name}
-            serachDescription={searchText.description}
-            serachCategory={searchText.category}
-            onchange={handleChange}
-            handleSearch={handleSearch}
-        />
-    );
+    switch (filteredFunko.state) {
+        case 'loading':
+            return (
+                <>
+                    <h1>loading...</h1>
+                </>
+            )
+        case 'error':
+            return (
+                <>
+                    <h1>{filteredFunko.state}</h1>
+                    <p>{filteredFunko.message}</p>
+                </>
+            )
+        case 'success':
+            return (
+                <>
+                    <SearchBarUi
+                        serachName={searchText.name}
+                        serachDescription={searchText.description}
+                        serachCategory={searchText.category}
+                        onchange={handleChange}
+                        handleSearch={handleSearch}
+                    />
+
+                    <div className="container">
+                        {filteredFunko.data.map(item => (
+                            <>
+                                {item.name}
+                            </>
+                        ))}
+                    </div>
+                </>
+            );
+    }
 }
