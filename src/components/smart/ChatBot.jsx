6dc6284f025e1c18e46441041efc399c4fd0c790
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-17 04:33:21 +0200

    refactor: enhance ChatBot component with improved answer rendering and conversation state management

--- src/components/smart/ChatBot.jsx
+++ src/components/smart/ChatBot.jsx
@@ -12,9 +12,11 @@ export default function ChatBot({ onClose }) {
         state: 'waiting'
     })
 
+    const [conversation, setConversation] = useState([])
+
     function handleSubmit() {
         console.log('submitChatBot');
-
+        setConversation([...conversation, ...question])
         setAnswer({
             state: 'loading',
             message: 'Thinking...'
@@ -58,38 +60,46 @@ export default function ChatBot({ onClose }) {
     function renderAnswer() {
         switch (answer.state) {
             case 'waiting':
-                return <div>HI! how can i help you?</div>
+                return (<div>HI! how can i help you?</div>)
             case 'loading':
                 return <div><Loader /></div>
             case 'error':
-                return <div>There was an error rendering the answer: {answer.message}</div>
+                return (<div>There was an error rendering the answer: {answer.message}</div>)
             case 'empty':
-                return <div>No answers found</div>
+                return (<div>No answers found</div>)
             case 'success':
                 switch (answer.answer.state) {
                     case 'json':
-                        return <div className={styles.sent_text}>
-                            <ul className='list-unstyled'>
-                                <h5>Here is what I've found</h5>
-                                {answer.answer.results.map(item => (
-                                    <>
-                                        <Link to={`/${item.slug.toLowerCase().replaceAll(' ', '-').replaceAll('(', '').replaceAll(')', '')}`}>
-                                            <ul className='col-8 my-2 p-2 border w-100 rounded-2'>
-                                                <div className="row">
-                                                    <div className="col-8">
-                                                        {item.name}
-                                                    </div>
-                                                    <div className="col-4">
-                                                        {item.price}
-                                                    </div>
-                                                </div>
-                                            </ul>
-                                        </Link>
-                                    </>
-                                ))}
-                            </ul>
+                        if (answer.answer.results.length == 0) {
+                            return (<p>Looks Like there are no releated products to your research :(</p>)
+                        }
+                        return (
+                            <>
+                                <div className={styles.sent_text} >
+                                    <ul className='list-unstyled'>
+                                        <h5>Here is what I've found</h5>
+                                        {answer.answer.results.map(item => (
+                                            <>
+                                                <Link to={`/${item.slug.toLowerCase().replaceAll(' ', '-').replaceAll('(', '').replaceAll(')', '')}`}>
+                                                    <ul className='col-8 my-2 p-2 border w-100 rounded-2'>
+                                                        <div className="row">
+                                                            <div className="col-8">
+                                                                {item.name}
+                                                            </div>
+                                                            <div className="col-4">
+                                                                {item.price} €
+                                                            </div>
+                                                        </div>
+                                                    </ul>
+                                                </Link>
+                                            </>
+                                        ))}
+                                    </ul>
 
-                        </div>;
+                                </div >
+                            </>
+                        )
+
                     case 'not-a-json-fallback':
                         return <div>{answer.answer.results}</div>
                     case 'not-a-json-failed':
@@ -118,10 +128,10 @@ export default function ChatBot({ onClose }) {
 
             <div className={styles.chat_container}>
                 <div className={styles.chat_messages}>
-                    <p className={styles.sent_text}>
+                    {/* <p className={styles.sent_text}>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae cumque voluptatem, ut eum hic excepturi </p>
                     <p className={styles.sent_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, veniam cupiditate obcaecati voluptates quia nam inventore libero eius sed expedita?</p>
-                    <p className={styles.sent_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita qui fuga officia deserunt voluptates perferendis incidunt suscipit deleniti quas voluptas!</p>
+                    <p className={styles.sent_text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita qui fuga officia deserunt voluptates perferendis incidunt suscipit deleniti quas voluptas!</p> */}
                     {/* Qui andranno i messaggi */}
                     {
                         renderAnswer()
