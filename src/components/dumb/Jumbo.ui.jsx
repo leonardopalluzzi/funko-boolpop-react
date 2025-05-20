2fdce4a27996bcced49e99af0b77df2417154c7e
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-21 01:13:16 +0200

    fix: refactor Jumbo component layout for improved responsiveness and styling

--- src/components/dumb/Jumbo.ui.jsx
+++ src/components/dumb/Jumbo.ui.jsx
@@ -5,14 +5,19 @@ export default function JumboUi({ productList, slideIndex, setIndex }) {
         <>
 
             <div className="jumbo_slide">
-                <Link className="img_jumbo_container" to={`/${productList[slideIndex].slug}`}>
-                    <img className="jumbo_img" src={`http://localhost:3000/${productList[slideIndex].banner}`} alt="" />
-                </Link>
-
-                <div className="jumbo_info">
-                    <h1 className="jumbo-title d-none d-lg-block">{productList[slideIndex].name}</h1>
-                    {/* <p className="pb-4 me-1 fs-6 d-none d-lg-block">{productList[slideIndex].description}</p> */}
+                <div className="row row-cols-1 row-cols-lg-2 align-items-center w-100 h-100 gap-5 m-auto">
+                    <div className="col col-lg-3 jumbo_info">
+                        <h1 className="jumbo-title">{productList[slideIndex].name}</h1>
+                        <p className="pb-4 me-1 fs-6 d-none d-lg-block">{productList[slideIndex].description}</p>
+                        <button className="btn btn-primary my-2">Details</button>
+                    </div>
+                    <div className="col col-lg-8">
+                        <div className="img_jumbo_container">
+                            <img className="jumbo_img" src={`http://localhost:3000/${productList[slideIndex].banner}`} alt="" />
+                        </div>
+                    </div>
                 </div>
+
                 <div className="dots">
                     <ul className="list-unstyled">
                         {productList.map((dot, i) => (
