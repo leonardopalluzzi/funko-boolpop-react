678bb746da3b446999aec1418d679aa878becc12
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-21 03:23:48 +0200

    feat: enhance CardUi component with navigation link and improve styling; refactor CSS for better maintainability

--- src/components/dumb/CardUi/Card.ui.jsx
+++ src/components/dumb/CardUi/Card.ui.jsx
@@ -1,5 +1,6 @@
 import ProductImages from "../../smart/ProductImages"
 import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import style from './card.module.css'
 
 export default function CardUi({ images, name, price, license, promotions, slug }) {
@@ -8,42 +9,41 @@ export default function CardUi({ images, name, price, license, promotions, slug 
 
     return (
         <>
-            <div className="col">
-                <div onClick={() => navigate(`/${slug}`)
-                }>
-                    <div className={`card ${style.card_custom}`}>
-                        <div className="card-header border-0 p-0">
+            <div className="col h-100">
+                <div className={`card ${style.card_custom}`}>
+                    <div className="card-header border-0 p-0">
+                        <Link to={`/${slug}`}>
                             <ProductImages images={images} />
-                        </div>
-                        <div className="card-body border-0 p-3">
-                            <span className="fs-8"> {license.name.toUpperCase()}</span>
-                            <h4 className="pb-2 card-title">{name}</h4>
-                            <div className="price_section_card">
-                                {
-                                    promotions.length > 0 ?
-                                        (
-                                            <>
-                                                <label>
-                                                    <div className="fw-bold text-promo">{promotions[0].name}</div>
-                                                    <span className="fs-6 text-old-price">
-                                                        <del>{price}€</del>
-                                                    </span>
-                                                    <span className="price_label_card text-price">
-                                                        {
-                                                            (price * promotions[0].discount / 100).toFixed(2)
-                                                        }€
-                                                    </span>
-                                                </label>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <label className="price_label_card text-price">
-                                                    {funko.result.price}€
-                                                </label>
-                                            </>
-                                        )
-                                }
-                            </div>
+                        </Link>
+                    </div>
+                    <div className="card-body border-0">
+                        <span className={style.card_license}> {license.name.toUpperCase()}</span>
+                        <h4 className={style.card_title}>{name}</h4>
+                        <div>
+                            {
+                                promotions.length > 0 ?
+                                    (
+                                        <>
+                                            <label>
+                                                <div className={style.card_promo}>{promotions[0].name}</div>
+                                                <span className="fs-5 text-old-price">
+                                                    <del>{price}€</del>
+                                                </span>
+                                                <span className={style.card_total}>
+                                                    {
+                                                        (price * promotions[0].discount / 100).toFixed(2)
+                                                    }€
+                                                </span>
+                                            </label>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <label className={style.card_total}>
+                                                {funko.result.price}€
+                                            </label>
+                                        </>
+                                    )
+                            }
                         </div>
                     </div>
                 </div>
