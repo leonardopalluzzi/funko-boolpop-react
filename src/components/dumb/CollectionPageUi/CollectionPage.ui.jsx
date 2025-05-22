3c0c333f1ffecbeeae38160a5ce4ce88ae815297
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-23 01:36:37 +0200

    fix style collections page

--- src/components/dumb/CollectionPageUi/CollectionPage.ui.jsx
+++ src/components/dumb/CollectionPageUi/CollectionPage.ui.jsx
@@ -23,24 +23,27 @@ export default function CollectioPageUi({ pops, setLimit, setPage }) {
                 </div >
 
                 {/* lista cards  */}
-                <div className="container py-5">
+                <div className={style.section_bg}>
                     <div className={style.cards_container}>
                         {
                             pops.data.results.map(item => (
                                 <>
-                                    <div className={style.card_container}>
-                                        <div className='row row-cols-1 row-cols-md-2'>
-                                            <div className="col-12 col-md-3">
-                                                <CardUi images={item.images} name={item.name} price={item.price} license={item.license} promotions={item.promotions} slug={item.slug} />
+                                    <div className="container-fluid">
+                                        <div className={style.card_container}>
+                                            <div className='row row-cols-1 row-cols-md-2'>
+                                                <div className="col-12 col-md-3">
+                                                    <CardUi images={item.images} name={item.name} price={item.price} license={item.license} promotions={item.promotions} slug={item.slug} />
 
-                                            </div>
-                                            <div className='col-12 col-md-7'>
-                                                <p className='p-5 border rounded rounded-5'>{item.description}</p>
-                                                <h5 className='mt-4'>INFORMATIONS</h5>
-                                                <ul className='list-unstyled'>
-                                                    <li className='ms-4'><strong>CATEGORY: </strong>{item.category[0].name}</li>
-                                                    <li className='ms-4'><strong>ATTRIBUTES: </strong>{item.attributes.map(att => (<>{att.name.toUpperCase()}, </>))}</li>
-                                                </ul>
+                                                </div>
+                                                <div className='col-12 col-md-7'>
+                                                    <h3 className={style.item_title}>{item.name.toUpperCase()}</h3>
+                                                    <p className='p-5 border border-dark rounded rounded-5 my-4'>{item.description}</p>
+                                                    <h5 className='mt-4'>INFORMATIONS</h5>
+                                                    <ul className='list-unstyled'>
+                                                        <li className='ms-4'><strong>CATEGORY: </strong>{item.category[0].name}</li>
+                                                        <li className='ms-4'><strong>ATTRIBUTES: </strong>{item.attributes.map(att => (<>{att.name.toUpperCase()}, </>))}</li>
+                                                    </ul>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
