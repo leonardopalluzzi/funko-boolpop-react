f213dbf25a50b7ea88c27571589af4e7950956d1
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-23 02:52:14 +0200

    fix search-results

--- src/components/dumb/SearchResults.ui.jsx
+++ src/components/dumb/SearchResults.ui.jsx
@@ -3,21 +3,29 @@ import { Link } from "react-router-dom";
 export default function SearchResultsUi({ results }) {
   return (
     <>
-      <h5 className="text-white d-md-none">Risultati:</h5>
+
       <div className="search_results_container">
+
         <div className="search_results_body">
+          <h5 className="px-3 pt-3">Risultati:</h5>
           <ul className="">
             {results.map((item) => (
               <>
                 <li>
-                  <Link to={`/${item.slug}`}>
-                    <div className="d-flex gap-5 align-items-center">
-                      <img
-                        className="search_results_img"
-                        src={`http://localhost:3000/${item.images[0].image}`}
-                        alt={item.name}
-                      />
-                      <p>{item.name}</p>
+                  <Link className="d-block" to={`/${item.slug}`}>
+                    <div className="container-fluid">
+                      <div className="row row-cols-2 align-items-center">
+                        <div className="col-3">
+                          <img
+                            className="search_results_img"
+                            src={`http://localhost:3000/${item.images[0].image}`}
+                            alt={item.name}
+                          />
+                        </div>
+                        <div className="col-8">
+                          <p className="">{item.name}</p>
+                        </div>
+                      </div>
                     </div>
                   </Link>
                 </li>
